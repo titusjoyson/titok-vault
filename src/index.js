8a90ce3890aeeac33223e81f887d07a34d81e182
Titus Joyson <tj.joyson@gmail.com>
2021-09-02 22:04:21 +0530

    layout tweaks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './App.less';
+import App from './routs';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
---
+++ src/routs/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Home }  from '../screens/home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
